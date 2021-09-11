75b5ea2275f50eb17a51025311476dbe8c33e8a2
danil <efan.daniel2002@gmail.com>
2021-09-12 01:03:35 +0400

    danil opyat

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MusicShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
