39ed322f3943fb7c514292349a5e600478b2466d
Stafar82 <angelattwood@gmail.com>
2021-09-12 01:16:56 +0400

    eblan

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace MusicShop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello maksaim");
         }
     }
 }
